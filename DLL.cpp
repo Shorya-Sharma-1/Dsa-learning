604baa250ad6a31dc75c81f045ccda7a45bb20bd
Shorya Sharma <shoryasharma251920@gmail.com>
2026-08-30 15:13:42 +0530

    dubly LL structure

---
+++ DLL.cpp
@@ -0,0 +1,23 @@
+struct Node{
+
+    int data;
+    Node* next;
+    Node* back;
+
+    Node( int data1, Node* next1, Node* back1){
+        data=data1;
+        next=next1;
+        back=back1;
+    }
+
+    Node(int data1){
+        data=data1;
+        next = nullptr;
+        back= nullptr;
+    }
+
+};
+
+int main (){
+    return 0;
+}
